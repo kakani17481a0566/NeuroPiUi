1751ebea5d90a1a27c3e9eed5b810deb32771504
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-06-02 10:45:42 +0530

    updated

--- src/app/pages/dashboards/home/index.jsx
+++ src/app/pages/dashboards/home/index.jsx
@@ -1,5 +1,5 @@
 import { Page } from "components/shared/Page";
-import OrdersDatatableV2 from "app/pages/tables/orders-datatable-2"; // verify this path is correct
+import OrdersDatatableV2 from "app/pages/tables/UserList"; // verify this path is correct
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="transition-content w-full px-[var(--margin-x)] pt-5 lg:pt-6">
         <div className="min-w-0">
           <h2 className="truncate text-xl font-medium tracking-wide text-gray-800 dark:text-dark-50">
-            Orders Table
+            User List
           </h2>
           
           <div className="mt-4"> {/* Added margin top for better spacing */}
