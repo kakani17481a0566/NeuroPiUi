fb3756034b424170e0562512e98da232110123b1
saivardhangone <saivardhangone@gmail.com>
2025-06-06 14:51:18 +0530

    updated weeklyplan at academics pages

--- src/app/pages/academics/weeklyplan/index.jsx
+++ src/app/pages/academics/weeklyplan/index.jsx
@@ -1,16 +1,17 @@
 import { Page } from "components/shared/Page";
-import TermPlan from "app/pages/tables/TermPlan";
+// import TermPlan from "app/pages/tables/TermPlan";
+import WeeklyPlan from "app/pages/tables/WeeklyPlan";
 export default function weeklyplan(){
     return (
      <Page title="Homepage">
           <div className="transition-content w-full px-[var(--margin-x)] pt-5 lg:pt-6">
             <div className="min-w-0">
               <h2 className="truncate text-xl font-medium tracking-wide text-gray-800 dark:text-dark-50">
-                WeeklyPlan
+                Academics
               </h2>
               
               <div className="mt-4"> {/* Added margin top for better spacing */}
-                <TermPlan/>
+                <WeeklyPlan/>
               </div>
             </div>
           </div>
