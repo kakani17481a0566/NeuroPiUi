73790df5d3cbb50c3649cbb8adad08d02641fc1e
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-06-08 09:25:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
-import eslint from 'vite-plugin-eslint';
+import eslint from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), jsconfigPaths(), svgr(),
-  eslint(), tailwindcss(),
+  plugins: [
+    react(),
+    jsconfigPaths(),
+    svgr(),
+    eslint(),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, 'src/app'),
+      components: path.resolve(__dirname, 'src/components'), // Optional
+    },
+  },
 })
