663a34d0e577f77b333af6a6c0d5e1370a3e3f4c
SaivardhanClasses <bablugone4@gmail.com>
2025-06-25 17:31:25 +0530

    navigation added

--- src/app/navigation/index.js
+++ src/app/navigation/index.js
@@ -1,11 +1,19 @@
+// import { useState } from "react";
 import { academics } from "./academics";
 import { dashboards } from "./dashboards";
 import { UserManagement } from "./UserManagement";
+import { getSessionData } from "utils/sessionStorage";
+const details=getSessionData();
+// eslint-disable-next-line react-hooks/rules-of-hooks
+let navigation=[];
+console.log(details);
+if(details.role=="Teacher"){
+   navigation=[academics]
+}
+else{
+    navigation=[UserManagement,dashboards];
+}
 
-export const navigation = [
-    dashboards,
-    academics,
-    UserManagement
-]
 
+export { navigation };
 export { baseNavigation } from './baseNavigation'
